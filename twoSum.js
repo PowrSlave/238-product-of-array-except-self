b26167d0321f10136d81775b95a028e9ca0749ac
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2021-10-17 14:41:59 -0600

    setup to try and optimize for O(n)

--- twoSum.js
+++ twoSum.js
@@ -6,7 +6,7 @@
 
 //the least efficient time complexity solution of O(n^2)
 
- var twoSum = function(nums, target) {
+var twoSum = function(nums, target) {
     for (var i=0; i<nums.length; i++) {
         for (var j=i+1; j<nums.length; j++) {
             if (nums[i] + nums[j] === target) {
@@ -16,4 +16,10 @@
     }
 };
 
-module.exports = twoSum
+// optimization #1 O(n) complexity
+
+var twoSum2 = function(nums, target) {
+    //maybe a forEach over nums to subtract each num from target?
+};
+
+module.exports = twoSum, twoSum2
