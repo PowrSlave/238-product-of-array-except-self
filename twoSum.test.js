925216ee40ce20334f9af49b6f5bbff39e6bf82e
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2021-10-17 14:33:35 -0600

    added two more test cases which should fail with the current solution?

--- twoSum.test.js
+++ twoSum.test.js
@@ -4,4 +4,10 @@ describe("TwoSum tests", () => {
     test('Because nums[0] + nums[1] == 9, we should return [0, 1].', () => {
         expect(twoSum([2,7,11,15], 9)).toEqual([0,1])
     })
+    test('Because nums[1] + nums[2] == 6, we should return [1, 2].', () => {
+        expect(twoSum([3,2,4], 6)).toEqual([1,2])
+    })
+    test('Because nums[0] + nums[1] == 6, we should return [0, 1].', () => {
+        expect(twoSum([3,3], 6)).toEqual([0,1])
+    })
 })
