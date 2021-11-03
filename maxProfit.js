790549037c3122256766e54cdbc8fe1e02ca6ed6
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2021-11-03 13:29:23 -0600

    single pass solution solved

--- maxProfit.js
+++ maxProfit.js
@@ -2,8 +2,28 @@
  * @param {number[]} prices
  * @return {number}
  */
- var maxProfit = function(prices) {
-    return -1
+
+//single pass solution
+var maxProfit = function(prices) {
+
+    if (prices.length < 2) {
+        return 0
+    }
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    //[3,2,6,5,0,3]
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice)
+        } else {
+            minPrice = Math.min(minPrice, prices[i])
+        }
+    }
+
+    return maxProfit
 };
 
 module.exports = maxProfit
--- maxProfit.test.js
+++ maxProfit.test.js
@@ -7,4 +7,11 @@ describe("maxProfit test cases", () => {
     test('In this case, no transactions are done and the max profit = 0.', () => {
         expect(maxProfit([7,6,4,3,1])).toEqual(0)
     })
+    test('Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.', () => {
+        expect(maxProfit([3,2,6,5,0,3])).toEqual(4)
+    })
+    test('there is only one day, so profit is 0', () => {
+        expect(maxProfit([4])).toEqual(0)
+    })
+
 })
