03f7f679d2d2657f1f72d9a3e5a8de2d33f8cb77
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2021-11-03 14:14:25 -0600

    implemented a hash map solution that isn't technically a hash map but  anyway it is slightly faster on smaller arrays than sorting and then iteratingn that is slightly faster

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -5,17 +5,32 @@
 
 //return true if array has duplicate values and false if it does not
 
-var containsDuplicate = function(nums) {
-    //[1,2,3,1]
-    nums.sort(function(a, b){return a-b})
-    //[1,1,2,3]
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i-1]) {
-            return true
-        }
-    }
+/* Solution 1: quick fast and loose attempt. */
 
+// var containsDuplicate = function(nums) {
+//     //[1,2,3,1]
+//     nums.sort(function(a, b){return a-b})
+//     //[1,1,2,3]
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] === nums[i-1]) {
+//             return true
+//         }
+//     }
+
+//     return false
+// };
+
+/* Solution 2: slightly faster via the use of a map instead of sorting values first */
+
+var containsDuplicate = function(nums) {
+    const map = {}
+    for(const num of nums) {
+        // If we have seen this num before return true
+        if(map[num]) return true
+        map[num] = true
+    }
     return false
 };
+
 
 module.exports = containsDuplicate
